335dc4ec55e64587fc5b196ca57191d07936a218
Thallys Cézar <75546628+ThallysCezar@users.noreply.github.com>
2024-06-25 09:09:39 -0300

    Delete ms-comunication directory, to rename again

--- ms-comunication/src/test/java/dev/thallys/ms/pagamento/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package dev.thallys.ms.pagamento;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
